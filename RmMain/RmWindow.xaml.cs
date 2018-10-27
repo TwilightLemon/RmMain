8e7ac38d60d336a5bf24ef04a64cc8d123643e85
Twilight <cz241126@live.com>
2018-10-27 23:00:49 +0800

    添加项目文件。

---
+++ RmMain/RmWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using RmMain.Properties;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace RmMain
+{
+    /// <summary>
+    /// RmWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class RmWindow : Window
+    {
+        Timer t = new Timer();
+        public RmWindow()
+        {
+            InitializeComponent();
+            t.Interval = Settings.Default.Spend;
+            t.Tick += delegate {
+                if (forInt != Settings.Default.Count)
+                {
+                    forInt++;
+                    tb.Text = forInt.ToString();
+                }
+                else forInt = 0;
+            };
+        }
+        private int forInt = 0;
+        private bool isRunning = false;
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isRunning)
+                t.Stop();
+            else t.Start();
+            isRunning = !isRunning;
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var ani = Resources["Stop"] as Storyboard;
+            ani.Completed += (s, ex) => { Close(); };
+            ani.Begin();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
---
+++ RmMain/SettingsWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using RmMain.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RmMain
+{
+    /// <summary>
+    /// SettingsWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+            jd.Value=Settings.Default.Spend;
+            ps.Text = Settings.Default.Count.ToString();
+            jd.ValueChanged += Slider_ValueChanged;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Settings.Default.Spend = (int)jd.Value;
+        }
+
+        private void ps_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(int.TryParse(ps.Text, out int i))
+                Settings.Default.Count=i;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Settings.Default.Save();
+        }
+    }
+}
